03675034e9caf30a6e8df8a80e6805b1510b363e
Matt Morgan <m@getplatypi.com>
2014-04-29 14:17:45 -0500

    branch merge test

--- framework/app/viewcontrols/main/main.viewcontrol.ts
+++ framework/app/viewcontrols/main/main.viewcontrol.ts
@@ -1,10 +1,10 @@
 module app {
     export class MainViewControl extends plat.ui.ViewControl {
+        templateUrl = 'viewcontrols/main/main.viewcontrol.html';
         context = {
             href: 'http://google.com'
         };
         hello = true;
-        templateUrl = 'viewcontrols/main/main.viewcontrol.html';
     }
 
     plat.register.viewControl('viewcontrol', MainViewControl, undefined, ['/', ':test/page(/:baz)(/*path)']);
