a2530ffdaa704b7e179a93daee945efdebae0190
Will Johnston <will.johnston@outlook.com>
2014-06-09 11:28:09 -0500

    added attribute control tests

---
+++ framework/testing/tests/controls/attributecontrol/attributecontrolfactory.test.ts
@@ -0,0 +1,24 @@
+﻿module tests.controls.attributeControlFactory {
+    var AttributeControlFactory = plat.acquire(plat.controls.IAttributeControlFactory);
+
+    describe('AttributeControlFactory Tests', () => {
+        var control: plat.controls.IAttributeControl;
+
+        beforeEach(() => {
+            control = AttributeControlFactory.getInstance();
+        });
+
+        it('should test getInstance', () => {
+            expect(control instanceof plat.controls.AttributeControl).toBe(true);
+        });
+
+        it('should test dispose', () => {
+            expect(control.priority).toBe(0);
+            expect(control.templateControl).toBeNull();
+
+            AttributeControlFactory.dispose(control);
+
+            expect(control.templateControl).toBeUndefined();
+        });
+    });
+}
