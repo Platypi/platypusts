e1f9af1ef0db0b7e4722aed3884b80823c1ed032
Will Johnston <will@getplatypi.com>
2014-06-11 10:05:31 -0500

    Added element property control tests

---
+++ framework/testing/tests/controls/elementpropertycontrol/elementpropertycontrol.test.ts
@@ -0,0 +1,74 @@
+﻿module tests.controls.elementPropertyControl {
+    describe('ElementPropertyControl Tests', () => {
+        var control: plat.controls.ElementPropertyControl,
+            parent: plat.ui.ITemplateControl,
+            ControlFactory = plat.acquire(plat.IControlFactory);
+
+        beforeEach(() => {
+            control = new plat.controls.ElementPropertyControl();
+            parent = new plat.ui.TemplateControl();
+            parent.controls = [control];
+            parent.resources = plat.acquire(plat.ui.IResourcesFactory).getInstance();
+            control.parent = parent;
+            control.attributes = plat.acquire(plat.ui.IAttributesInstance);
+        });
+
+        afterEach(() => {
+            ControlFactory.dispose(control);
+            ControlFactory.dispose(parent);
+        });
+
+        it('should test setter with empty expression', () => {
+            control = new plat.controls.Href();
+            control.attributes = plat.acquire(plat.ui.IAttributesInstance);
+            var element: HTMLAnchorElement = control.element = <HTMLAnchorElement>control.dom.serializeHtml('<a plat-href=""></a>').childNodes[0];
+            control.property = 'href';
+            control.attribute = 'platHref';
+            control.attributes['platHref'] = '';
+
+            expect(element.href).toBe('');
+
+            control.setter();
+
+            expect(element.href).toBe('');
+        });
+
+        it('should test setter with null', () => {
+            control.property = 'href';
+            control.attribute = 'platHref';
+            control.attributes['platHref'] = 'foo';
+
+            control.setter();
+
+            expect(true).toBe(true);
+        });
+
+        it('should test setter with undefined property', () => {
+            var element: HTMLAnchorElement = control.element = <HTMLAnchorElement>control.dom.serializeHtml('<input plat-href="foo"></a>').childNodes[0];
+            control.property = 'href';
+            control.attribute = 'platHref';
+            control.attributes['platHref'] = 'foo';
+
+            expect(element.href).toBeUndefined();
+
+            control.setter();
+
+            expect(element.href).toBeUndefined();
+        });
+
+        it('should test setter', () => {
+            control = new plat.controls.Src();
+            control.attributes = plat.acquire(plat.ui.IAttributesInstance);
+            var element: HTMLAnchorElement = control.element = <HTMLAnchorElement>control.dom.serializeHtml('<a plat-href="foo"></a>').childNodes[0];
+            control.property = 'href';
+            control.attribute = 'platHref';
+            control.attributes['platHref'] = 'foo';
+
+            expect(element.href).toBe('');
+
+            control.setter();
+
+            expect(element.pathname).toBe('foo');
+        });
+    });
+}
