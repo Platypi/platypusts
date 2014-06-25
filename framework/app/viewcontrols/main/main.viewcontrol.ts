e7a9e2e2ec97fc1c78d0ab1681dafdff2a4e5e6f
Will Johnston <will@getplatypi.com>
2014-06-25 15:06:05 -0500

    fixed parser for array notation indexing

--- framework/app/viewcontrols/main/main.viewcontrol.ts
+++ framework/app/viewcontrols/main/main.viewcontrol.ts
@@ -11,8 +11,20 @@ module app {
                 { name: 'Paul' },
                 { name: 'Will' }
             ],
-            selected: 'M@'
+            indexed: {
+                val: 'foo'
+            },
+            selected: 'M@',
+            index: 'indexed'
         };
+        initialize() {
+            var parser = plat.acquire(plat.expressions.IParser);
+
+            var expr = parser.parse('context[index].val'),
+                out = expr.evaluate({ context: this.context, index: this.context.index });
+
+            out;
+        }
         navigatedTo(route: plat.web.IRoute<any>) {
             console.log((<any>Object).observe);
             if (route.path.length === 0) {
