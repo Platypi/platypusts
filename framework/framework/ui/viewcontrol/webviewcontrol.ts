2e6d25e2d431287ea0a75ec5e3afd33fc61dd11b
Will Johnston <will@getplatypi.com>
2014-08-13 17:36:10 -0500

    added description element to webviewcontrol, changed document to take in optional argument

--- framework/framework/ui/viewcontrol/webviewcontrol.ts
+++ framework/framework/ui/viewcontrol/webviewcontrol.ts
@@ -5,16 +5,50 @@
      * It also provides functionality for setting the title of a page.
      */
     export class WebViewControl extends BaseViewControl {
-        static titleElement = plat.acquire(plat.Document).head.querySelector('title');
+        static titleElement: HTMLTitleElement;
+        static descriptionElement: HTMLMetaElement;
 
         static setTitle(title: string) {
-            WebViewControl.titleElement.textContent = title.replace(/\//g, ' ');
+            var element = WebViewControl.titleElement;
+
+            if (!isNode(element)) {
+                var $document = plat.acquire(plat.Document);
+                element = WebViewControl.titleElement = <HTMLTitleElement>$document.head.querySelector('title');
+
+                if (!isNode(element)) {
+                    element = WebViewControl.titleElement = <HTMLTitleElement>$document.head.appendChild($document.createElement('title'));
+                }
+            }
+
+            element.textContent = title.replace(/\//g, ' ');
+        }
+
+        static setDescription(description: string) {
+            var element = WebViewControl.descriptionElement;
+
+            if (!isNode(element)) {
+                var $document = plat.acquire(plat.Document);
+                element = WebViewControl.descriptionElement = <HTMLMetaElement>$document.head.querySelector('meta[name="description"]');
+
+                if (!isNode(element)) {
+                    element = WebViewControl.descriptionElement = <HTMLMetaElement>$document.head.appendChild($document.createElement('meta'));
+                    element.setAttribute('name', 'description');
+                }
+            }
+
+            element.setAttribute('content', description.replace(/\//g, ' '));
         }
 
         /**
          * The title of the page, corresponds to the textContent of the title element in the HTML head.
          */
         title = '';
+
+        
+        /**
+         * The title of the page, corresponds to the content of the description meta element in the HTML head.
+         */
+        description = '';
 
         /**
          * Specifies the navigator for this control. Used for navigating to other IWebViewControls
@@ -25,11 +59,16 @@
         constructor() {
             super();
             this.on('navigated', () => {
-                if (this.title.length === 0) {
-                    return;
+                if (isEmpty(this.title)) {
+                    this.title = '';
+                }
+
+                if (isEmpty(this.description)) {
+                    this.description = '';
                 }
 
                 WebViewControl.setTitle(this.title);
+                WebViewControl.setDescription(this.description);
             });
         }
 
@@ -38,7 +77,16 @@
          */
         setTitle(title: string) {
             this.title = title;
-            WebViewControl.setTitle(this.title);
+            WebViewControl.setTitle(title);
+        }
+
+        /**
+         * Allows the IWebViewControl set its description programmatically and 
+         * have it reflect in the browser meta description tag.
+         */
+        setDescription(description: string) {
+            this.description = description;
+            WebViewControl.setDescription(description);
         }
     }
 
