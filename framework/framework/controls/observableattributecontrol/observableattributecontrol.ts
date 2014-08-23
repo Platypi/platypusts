f8baa9be159afdd372a6763e73dfc5c25480bc3c
Will Johnston <will@getplatypi.com>
2014-08-23 16:19:54 -0500

    finished observableattribute docs

--- framework/framework/controls/observableattributecontrol/observableattributecontrol.ts
+++ framework/framework/controls/observableattributecontrol/observableattributecontrol.ts
@@ -1,39 +1,106 @@
 ﻿module plat.controls {
     /**
-     * An AttributeControl that deals with observing changes for a specified property.
+     * @name ObservableAttributeControl
+     * @memberof plat.controls
+     * @kind class
+     * 
+     * @extends {plat.controls.AttributeControl}
+     * @implements {plat.controls.IObservableAttributeControl}
+     * 
+     * @description
+     * An {@link plat.controls.AttributeControl|AttributeControl} that deals with observing changes for a specified property.
      */
     export class ObservableAttributeControl extends AttributeControl implements IObservableAttributeControl {
+        /**
+         * @name $ContextManagerStatic
+         * @memberof plat.controls.ObservableAttributeControl
+         * @kind property
+         * @access public
+         * 
+         * @type {plat.observable.IContextManagerStatic}
+         * 
+         * @description
+         * Reference to the {@link plat.observable.IContextManagerStatic|IContextManagerStatic} injectable.
+         */
         $ContextManagerStatic: observable.IContextManagerStatic = acquire(__ContextManagerStatic);
 
         /**
+         * @name property
+         * @memberof plat.controls.ObservableAttributeControl
+         * @kind property
+         * @access public
+         * 
+         * @type {string}
+         * 
+         * @description
          * The property to set on the associated template control.
          */
         property: string = '';
 
         /**
+         * @name attribute
+         * @memberof plat.controls.ObservableAttributeControl
+         * @kind property
+         * @access public
+         * 
+         * @type {string}
+         * 
+         * @description
          * The camel-cased name of the control as it appears as an attribute.
          */
         attribute: string;
 
         /**
+         * @name priority
+         * @memberof plat.controls.ObservableAttributeControl
+         * @kind property
+         * @access public
+         * 
+         * @type {number}
+         * 
+         * @description
          * This control needs to load before its templateControl
          */
         priority = 200;
 
         /**
+         * @name _listeners
+         * @memberof plat.controls.ObservableAttributeControl
+         * @kind property
+         * @access protected
+         * 
+         * @type {Array<plat.IPropertyChangedListener>}
+         * 
+         * @description
          * The set of functions added by the Template Control that listens 
          * for property changes.
          */
         _listeners: Array<(newValue: any, oldValue?: any) => void> = [];
 
         /**
+         * @name _removeListener
+         * @memberof plat.controls.ObservableAttributeControl
+         * @kind property
+         * @access protected
+         * 
+         * @type {IRemoveListener}
+         * 
+         * @description
          * The function to stop listening for property changes.
          */
         _removeListener: IRemoveListener;
 
         /**
+         * @name initialize
+         * @memberof plat.controls.ObservableAttributeControl
+         * @kind function
+         * @access public
+         * 
+         * @description
          * Sets the initial value of the property on 
          * the Template Control.
+         * 
+         * @returns {void}
          */
         initialize(): void {
             this.attribute = camelCase(this.type);
@@ -41,7 +108,15 @@
         }
 
         /**
+         * @name loaded
+         * @memberof plat.controls.ObservableAttributeControl
+         * @kind function
+         * @access public
+         * 
+         * @description
          * Observes the property and resets the value.
+         * 
+         * @returns {void}
          */
         loaded(): void {
             this._observeProperty();
@@ -49,9 +124,17 @@
         }
 
         /**
+         * @name dispose
+         * @memberof plat.controls.ObservableAttributeControl
+         * @kind function
+         * @access public
+         * 
+         * @description
          * Stops listening for changes to the evaluated 
          * expression and removes references to the listeners 
          * defined by the Template Control.
+         * 
+         * @returns {void}
          */
         dispose(): void {
             if (isFunction(this._removeListener)) {
@@ -62,10 +145,18 @@
         }
 
         /**
+         * @name _setProperty
+         * @memberof plat.controls.ObservableAttributeControl
+         * @kind function
+         * @access protected
+         * 
+         * @description
          * Sets the property on the Template Control.
          * 
-         * @param value The new value of the evaluated expression.
-         * @param oldValue The old value of the evaluated expression.
+         * @param {any} value The new value of the evaluated expression.
+         * @param {any} oldValue? The old value of the evaluated expression.
+         * 
+         * @returns {void}
          */
         _setProperty(value: any, oldValue?: any): void {
             var templateControl = this.templateControl;
@@ -82,12 +173,20 @@
         }
 
         /**
+         * @name _callListeners
+         * @memberof plat.controls.ObservableAttributeControl
+         * @kind function
+         * @access protected
+         * 
+         * @description
          * Calls the listeners defined by the Template Control.
          * 
-         * @param value The new value of the evaluated expression.
-         * @param oldValue The old value of the evaluated expression.
+         * @param {any} value The new value of the evaluated expression.
+         * @param {any} oldValue The old value of the evaluated expression.
+         * 
+         * @returns {void}
          */
-        _callListeners(newValue: any, oldValue?: any): void {
+        _callListeners(newValue: any, oldValue: any): void {
             var listeners = this._listeners,
                 length = listeners.length,
                 templateControl = this.templateControl;
@@ -98,11 +197,17 @@
         }
 
         /**
+         * @name _addListener
+         * @memberof plat.controls.ObservableAttributeControl
+         * @kind function
+         * @access protected
+         * 
+         * @description
          * Adds a listener as defined by the Template Control.
          * 
-         * @param listener The listener added by the Template Control.
+         * @param {plat.IPropertyChangedListener} listener The listener added by the Template Control.
          */
-        _addListener(listener: (newValue: any, oldValue?: any) => void): IRemoveListener {
+        _addListener(listener: (newValue: any, oldValue: any) => void): IRemoveListener {
             var listeners = this._listeners,
                 index = listeners.length;
 
@@ -114,7 +219,15 @@
         }
 
         /**
+         * @name _getValue
+         * @memberof plat.controls.ObservableAttributeControl
+         * @kind function
+         * @access protected
+         * 
+         * @description
          * Evaluates the attribute's value.
+         * 
+         * @returns {any}
          */
         _getValue(): any {
             var expression = (<any>this.attributes)[this.attribute],
@@ -128,7 +241,15 @@
         }
 
         /**
+         * @name _observeProperty
+         * @memberof plat.controls.ObservableAttributeControl
+         * @kind function
+         * @access protected
+         * 
+         * @description
          * Observes the attribute's value.
+         * 
+         * @returns {void}
          */
         _observeProperty(): void {
             var expression = (<any>this.attributes)[this.attribute],
@@ -143,22 +264,64 @@
     }
 
     /**
-     * Describes an attribute object that deals with observing changes for a specified property.
+     * @name IObservableAttributeControl
+     * @memberof plat.controls
+     * @kind interface
+     * 
+     * @extends {plat.controls.IAttributeControl}
+     * 
+     * @description
+     * An {@link plat.controls.IAttributeControl|IAttributeControl} that deals with observing changes for a specified property.
      */
     export interface IObservableAttributeControl extends IAttributeControl {
         /**
+         * @name property
+         * @memberof plat.controls.IObservableAttributeControl
+         * @kind property
+         * @access public
+         * 
+         * @type {string}
+         * 
+         * @description
          * The property to set on the associated template control.
          */
         property: string;
 
         /**
+         * @name attribute
+         * @memberof plat.controls.IObservableAttributeControl
+         * @kind property
+         * @access public
+         * 
+         * @type {string}
+         * 
+         * @description
          * The camel-cased name of the control as it appears as an attribute.
          */
         attribute: string;
     }
 
+    /**
+     * @name Options
+     * @memberof plat.controls
+     * @kind class
+     * 
+     * @extends {plat.controls.ObservableAttributeControl}
+     * 
+     * @description
+     * An {@link plat.controls.ObservableAttributeControl|ObservableAttributeControl} that sets 'options' as the 
+     * associated property.
+     */
     export class Options extends ObservableAttributeControl {
         /**
+         * @name property
+         * @memberof plat.controls.Options
+         * @kind property
+         * @access public
+         * 
+         * @type {string}
+         * 
+         * @description
          * The property to set on the associated template control.
          */
         property: string = 'options';
