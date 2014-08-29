43d9f9c2aa07fce3d4b8d48ebe744062c45d8f65
Matt Morgan <m@getplatypi.com>
2014-08-29 11:30:55 -0500

    finished all storage docs

--- framework/framework/storage/sessionstorage.ts
+++ framework/framework/storage/sessionstorage.ts
@@ -1,9 +1,28 @@
 module plat.storage {
     /**
+     * @name SessionStorage
+     * @memberof plat.storage
+     * @kind class
+     * 
+     * @extends {plat.storage.BaseStorage}
+     * @implements {plat.storage.ISessionStorage}
+     * 
+     * @description
      * A class for wrapping SessionStorage as an injectable.
      */
     export class SessionStorage extends BaseStorage implements ISessionStorage {
         /* tslint:disable:no-unused-variable */
+        /**
+         * @name __storage
+         * @memberof plat.storage.LocalStorage
+         * @kind property
+         * @access private
+         * 
+         * @type {Storage}
+         * 
+         * @description
+         * Reference to HTML5 sessionStorage.
+         */
         private __storage: Storage = (<Window>acquire(__Window)).sessionStorage;
         /* tslint:enable:no-unused-variable */
     }
@@ -16,52 +35,106 @@ module plat.storage {
     }
 
     register.injectable(__SessionStorage, ISessionStorage);
-
+    
     /**
+     * @name ISessionStorage
+     * @memberof plat.storage
+     * @kind interface
+     * 
+     * @description
      * Describes an object used to wrap session storage into an injectable.
      */
     export interface ISessionStorage {
         /**
+         * @name length
+         * @memberof plat.storage.ISessionStorage
+         * @kind property
+         * @access public
+         * @readonly
+         * 
+         * @type {number}
+         * 
+         * @description
          * Returns the number of items in sessionStorage.
          */
         length: number;
-
+        
         /**
+         * @name clear
+         * @memberof plat.storage.ISessionStorage
+         * @kind function
+         * @access public
+         * 
+         * @description
          * Clears sessionStorage, deleting all of its keys.
+         * 
+         * @returns {void}
          */
         clear(): void;
-
+        
         /**
-         * Gets an item out of session storage with the assigned key.
+         * @name getItem
+         * @memberof plat.storage.ISessionStorage
+         * @kind function
+         * @access public
          * 
-         * @param key The key of the item to retrieve from sessionStorage.
-         * @returns {T} The item retrieved from sessionStorage.
+         * @description
+         * Gets an item out of sessionStorage with the assigned key.
+         * 
+         * @param {string} key The key of the item to retrieve from sessionStorage.
+         * 
+         * @typeparam {any} T The type of item being retrieved.
+         * 
+         * @returns {T} The item retrieved from storage.
          */
         getItem<T>(key: string): T;
-
+        
         /**
+         * @name key
+         * @memberof plat.storage.ISessionStorage
+         * @kind function
+         * @access public
+         * 
+         * @description
          * Allows for iterating over sessionStorage keys with an index. When
          * called with an index, it will return the key at that index in 
          * sessionStorage.
          * 
-         * @param index The index used to retrieve the associated key.
+         * @param {number} index The index used to retrieve the associated key.
+         * 
          * @returns {string} The key at the given index.
          */
         key(index: number): string;
-
+        
         /**
+         * @name removeItem
+         * @memberof plat.storage.ISessionStorage
+         * @kind function
+         * @access public
+         * 
+         * @description
          * Searches in sessionStorage for an item and removes it if it 
          * exists.
          * 
-         * @param key the Key of the item to remove from sessionStorage.
+         * @param {string} key The key of the item to remove from storage.
+         * 
+         * @returns {void}
          */
         removeItem(key: string): void;
-
+        
         /**
+         * @name setItem
+         * @memberof plat.storage.ISessionStorage
+         * @kind function
+         * @access public
+         * 
+         * @description
          * Adds data to sessionStorage with the designated key.
          * 
-         * @param key The key of the item to store in sessionStorage.
-         * @param data The data to store in sessionStorage with the key.
+         * @param {string} key The key of the item to store in sessionStorage.
+         * @param {any} data The data to store in sessionStorage with the key.
+         * 
+         * @returns {void}
          */
         setItem(key: string, data: any): void;
     }
