929c5844cab3b542280b7d8f8023baf19fa2e565
Will Johnston <will@getplatypi.com>
2015-02-23 09:38:42 -0600

    incrementing to minro version

--- framework/testing/framework/platypus.d.ts
+++ framework/testing/framework/platypus.d.ts
@@ -1,5 +1,5 @@
 /**
-  * PlatypusTS v0.11.3 (http://getplatypi.com)
+  * PlatypusTS v0.12.0 (http://getplatypi.com)
   * Copyright 2015 Platypi, LLC. All rights reserved.
   * PlatypusTS is licensed under the GPL-3.0 found at
   * http://opensource.org/licenses/GPL-3.0
