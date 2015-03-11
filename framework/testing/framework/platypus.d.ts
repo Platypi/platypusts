c33c739fc0e4f5add6214462b6ce97dcdf4295a5
Will Johnston <will@getplatypi.com>
2015-03-11 10:19:37 -0500

    declaration

--- framework/testing/framework/platypus.d.ts
+++ framework/testing/framework/platypus.d.ts
@@ -1,5 +1,5 @@
 /**
-  * PlatypusTS v0.12.2 (http://getplatypi.com)
+  * PlatypusTS v0.12.3 (http://getplatypi.com)
   * Copyright 2015 Platypi, LLC. All rights reserved.
   * PlatypusTS is licensed under the GPL-3.0 found at
   * http://opensource.org/licenses/GPL-3.0
@@ -8938,6 +8938,9 @@ declare module plat {
                   * Determines the proper link upon $tap.
                   */
                 protected _handleTap(ev: IGestureEvent): void;
+                /**
+                  * Calls to remove the click eater after a delay.
+                  */
                 dispose(): void;
             }
             /**
