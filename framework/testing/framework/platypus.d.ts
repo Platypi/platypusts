0e39fb0efe991b86f6cbee2f4040d526ba30196b
Will Johnston <will@getplatypi.com>
2015-03-20 21:42:47 -0500

    incrementing version

--- framework/testing/framework/platypus.d.ts
+++ framework/testing/framework/platypus.d.ts
@@ -1,5 +1,5 @@
 /**
-  * PlatypusTS v0.12.8 (http://getplatypi.com)
+  * PlatypusTS v0.12.9 (http://getplatypi.com)
   * Copyright 2015 Platypi, LLC. All rights reserved.
   *
   * PlatypusTS is licensed under the GPL-3.0 found at
