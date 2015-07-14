7dfc8af91462c01301bbd7f55105caf748ffddad
Will Johnston <will@getplatypi.com>
2015-07-14 15:04:49 -0500

    dist

--- dist/platypus.d.ts
+++ dist/platypus.d.ts
@@ -1,5 +1,5 @@
 /**
-  * PlatypusTS v0.13.15 (https://platypi.io)
+  * PlatypusTS v0.13.16 (https://platypi.io)
   * Copyright 2015 Platypi, LLC. All rights reserved.
   *
   * PlatypusTS is licensed under the MIT license found at
