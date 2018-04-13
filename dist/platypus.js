1397436ec193b974342b61dcb5df712ff596c1c8
William Johnston <johnston.w@gmail.com>
2018-04-13 11:47:17 -0500

    dist

--- dist/platypus.js
+++ dist/platypus.js
@@ -11,7 +11,7 @@ var __extends = (this && this.__extends) || (function () {
 })();
 /* tslint:disable */
 /**
- * PlatypusTS v0.26.2 (https://platypi.io)
+ * PlatypusTS v0.26.3 (https://platypi.io)
  * Copyright 2015 Platypi, LLC. All rights reserved.
  *
  * PlatypusTS is licensed under the MIT license found at
